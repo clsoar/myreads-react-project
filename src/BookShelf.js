5bb2587583e554d5b647a3ce0192c361332ca454
CherieLee-Mason <clsoar@gmail.com>
2018-08-01 11:26:05 -0500

    Got books and passed them to bookshelf

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -9,12 +9,16 @@ class BookShelf extends Component {
         <h2 className="bookshelf-title">Currently Reading</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book />
-            </li>
-            <li>
-              <Book />
-            </li>
+            {
+              this.props.books
+              .filter(book => book.shelf === 'currentlyReading')
+              .map(book => (
+                <li key={book.id}>
+                  <Book
+                  />
+                </li>
+              ))}
+
           </ol>
         </div>
       </div>
@@ -22,12 +26,15 @@ class BookShelf extends Component {
         <h2 className="bookshelf-title">Want to Read</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book />
-            </li>
-            <li>
-            <Book />
-            </li>
+          {
+            this.props.books
+            .filter(book => book.shelf === 'wantToRead')
+            .map(book => (
+              <li key={book.id}>
+                <Book
+                />
+              </li>
+            ))}
           </ol>
         </div>
       </div>
@@ -35,15 +42,15 @@ class BookShelf extends Component {
         <h2 className="bookshelf-title">Read</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book />
-            </li>
-            <li>
-              <Book />
-            </li>
-            <li>
-              <Book />
-            </li>
+          {
+            this.props.books
+            .filter(book => book.shelf === 'read')
+            .map(book => (
+              <li key={book.id}>
+                <Book
+                />
+              </li>
+            ))}
           </ol>
         </div>
       </div>
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import BookShelf from './BookShelf'
 
-
 class HomePage extends Component {
   render() {
     return (
+
       <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
           <div>
-            <BookShelf />
-
+            <BookShelf
+              books={this.props.books}
+              />
           </div>
         </div>
         <div className="open-search">
