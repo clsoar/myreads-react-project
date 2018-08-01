4370be5e9ee6a14e23ef27c77ce3ab494e35b2c8
CherieLee-Mason <clsoar@gmail.com>
2018-08-01 11:35:07 -0500

    Books sorted and passed info from API

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -15,6 +15,7 @@ class BookShelf extends Component {
               .map(book => (
                 <li key={book.id}>
                   <Book
+                    book={ book }
                   />
                 </li>
               ))}
@@ -32,6 +33,7 @@ class BookShelf extends Component {
             .map(book => (
               <li key={book.id}>
                 <Book
+                  book={ book }
                 />
               </li>
             ))}
@@ -48,6 +50,7 @@ class BookShelf extends Component {
             .map(book => (
               <li key={book.id}>
                 <Book
+                  book={ book }
                 />
               </li>
             ))}
